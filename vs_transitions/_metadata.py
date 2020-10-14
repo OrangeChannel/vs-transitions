13369a28e26bfcb2c41ce4522bc93e1b1990aa00
Dave <orangechannel@pm.me>
2020-10-14 13:03:38 -0400

    Date bump

--- vs_transitions/_metadata.py
+++ vs_transitions/_metadata.py
@@ -1,5 +1,5 @@
 from datetime import date
 
 __author__ = 'Dave <orangechannel@pm.me>'
-__date__ = date.fromisoformat('2020-08-28')
+__date__ = date.fromisoformat('2020-10-14')
 __version__ = '0.0.0'
